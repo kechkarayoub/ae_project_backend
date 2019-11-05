3ed9a45c9d10423d0cdca149c2f922c354f02007
mahboub <mahboub@example.com>
2019-11-05 08:32:42 +0100

    code update for serving

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -7,9 +7,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "full_app_db",
         "USER": "root",
-        "PASSWORD": "full_app_pws",
-        "HOST": "127.0.0.0",
-        "PORT": "3306"
+        "PASSWORD": "full_app_pwd",
+        "HOST": "",
+        "PORT": ""
     }
 }
 
