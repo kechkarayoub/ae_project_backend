5234167080ea4a80d6858440a0fbd72afb80c483
mahboub <mahboub@example.com>
2019-11-05 08:43:05 +0100

    code update for serving

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,6 @@
 
 try:
-    with open("backend/server_settings.json") as server_conf_file:
-        import pymysql
-        pymysql.install_as_MySQLdb()
+    import pymysql
+    pymysql.install_as_MySQLdb()
 except:
     pass
