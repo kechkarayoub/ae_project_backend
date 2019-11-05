7e35f8440796c94fe7cf96e4c114298f4302f3bc
mahboub <mahboub@example.com>
2019-11-05 08:41:08 +0100

    code update for serving

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -95,10 +95,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'full_app_db',
+        'USER': 'root',
+        'PASSWORD': 'full_app_pwd',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
