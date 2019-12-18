760ef038d38de6ae7cad4678cde6fc4760b5f3c9
mahboub <mahboub@example.com>
2019-12-18 08:11:43 +0100

    add account gmail for production and preproduction

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+try:
+    from .active_settings import ACTIVE_SETTINGS
+except:
+    ACTIVE_SETTINGS = "backend.settings.production"
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", ACTIVE_SETTINGS)
 
 application = get_wsgi_application()
