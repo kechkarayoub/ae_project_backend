85880b30aae550295757390c511067e3d4e76461
mahboub <mahboub@example.com>
2020-01-24 17:02:22 +0100

    remove celery code

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -57,21 +57,8 @@ INSTALLED_APPS = [
     'admin_data',
     'settings_db',
     'funding',
-    'dbbackup',
-    'djcelery'
+    'dbbackup'
 ]
-import djcelery
-djcelery.setup_loader()
-BROKER_URL = 'redis://localhost:6379/1'
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_BROKER_URL = 'amqp://localhost'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Canada/Eastern'
-CELERY_IMPORTS = ("settings_db.tasks")
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- settings_db/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-from celery.task.schedules import crontab
-from celery.decorators import periodic_task
-from celery.decorators import task
-from datetime import timedelta
-
-@shared_task
-def test_task():
-    print('........................')
