87f60c6e044d491ad6f73e75421295dc43651aba
kechkarayoub <kechkarayoub@gmail.com>
2020-02-15 15:25:51 +0000

    hosting problems

--- settings_db/tasks.py
+++ settings_db/tasks.py
@@ -4,7 +4,9 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.decorators import task
 from datetime import timedelta
-
+from backend.utils import send_email
+from django.conf import settings
 @app.task
 def test___task():
+    send_email("Test celery", "Celery work perfectly", settings.EMAIL_HOST_USER, "kechkarayoub@gmail.com")
     print('........................')
