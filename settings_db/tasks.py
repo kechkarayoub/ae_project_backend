d27f2d3244939d3e335bfef90ea04be47c72dbb2
kechkarayoub <kechkarayoub@gmail.com>
2020-03-15 21:47:18 +0000

    backup task

--- settings_db/tasks.py
+++ settings_db/tasks.py
@@ -5,9 +5,25 @@ from celery.decorators import periodic_task
 from celery.decorators import task
 from datetime import timedelta
 from backend.utils import send_email
+from django.core import management
 from django.conf import settings
 @app.task
 def test___task():
     print('test email task')
     send_email("Test celery", "Celery work perfectly", settings.EMAIL_HOST_USER, "kechkarayoub@gmail.com")
     print('........................')
+
+
+@app.task
+def backup_function():
+    kwargs = {}
+    if settings.ENVIRONMENT == "preproduction":
+        kwargs.update({
+            "settings": "backend.settings.preproduction"
+        })
+    elif settings.ENVIRONMENT == "production":
+        kwargs.update({
+            "settings": "backend.settings.production"
+        })
+    management.call_command('dbbackup', **kwargs)
+    management.call_command('mediabackup', **kwargs)
